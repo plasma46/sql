0272532fddc022ce0aae6373df0a6d84d9cfea9d
plasma46 <plasma46@yandex.ru>
2021-02-26 14:19:03 +0300

    Открывает подлючпение Закидывает в ДатаФрейм Закрывает Сохраняет в ексель на Рабочий стол

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
 import pandas as pd
-import pyodbc
+import sql_connection
+import sql_df
 
-server = 'SQL.Samorodov.SU'
-database = '(лаб) Банк Олега Тинькова (TCS)'
-username = 'Чуркин Дмитрий Юрьевич'
-password = 'Dima070193'
-
-cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
-a = int(input('Введите значения '))
-
-df = pd.read_sql_query(f"SELECT TOP 10*  FROM [3].[Customers] WHERE TSC_Customer_ID = {a} ",cnxn)
-print(df)
-
-pyodbc.pooling = False
 
--- sql_df.py
+++ sql_df.py
@@ -1,1 +1,9 @@
-writer = pd.ExcelWriter('C:\\Users\\dmchurkin\\Desktop\\output.xlsx', engine='xlsxwriter')
+import sql_connection
+import pandas as pd
+
+
+writer = pd.ExcelWriter('C:\\Users\\Dmich\\Desktop\\output.xlsx', engine='xlsxwriter')
+
+sql_connection.df.to_excel(writer, 'Sheet1')
+
+writer.save()
