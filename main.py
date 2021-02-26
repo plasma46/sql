ef60aa7e4b96adcfb6cf5f53b4af2a240c6d6811
plasma46 <plasma46@yandex.ru>
2021-02-26 13:56:51 +0300

    Закрытие подключения

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import pyodbc
-# Some other example server values are
-# server = 'localhost\sqlexpress' # for a named instance
-# server = 'myserver,port' # to specify an alternate port
+
 server = 'SQL.Samorodov.SU'
 database = '(лаб) Банк Олега Тинькова (TCS)'
 username = 'Чуркин Дмитрий Юрьевич'
@@ -10,9 +8,10 @@ password = 'Dima070193'
 
 cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-a = int(input('Введите значения'))
+a = int(input('Введите значения '))
 
 df = pd.read_sql_query(f"SELECT TOP 10*  FROM [3].[Customers] WHERE TSC_Customer_ID = {a} ",cnxn)
 print(df)
 
+pyodbc.pooling = False
 
